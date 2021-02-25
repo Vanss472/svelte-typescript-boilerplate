27488731e46df47b9e00ae6848eba04f4a4220b2
Vanessa Martinez <vanss472@gmail.com>
2021-02-25 17:29:08 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import Button from "./components/UI/Button/Button.svelte";
+
+	const handleClick = () => {
+		console.log('clicked')
+	}
+</script>
+
+<main>
+	<!-- The double {{curlies}} - is an object literal inside an expression tag (btn) -->
+	<Button btn={{
+		type: 'button',
+		onClick: handleClick,
+	}}>Default button</Button>
+</main>
+
+<style>
+	:root {
+		--p-text: #000;
+		--btn-bg: #fff;
+		--btn-border: #d9d9d9;
+		--btn-text-hover: #40a9ff;
+		--btn-border-hover: #40a9ff;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/UI/Button/Button.svelte
@@ -0,0 +1,63 @@
+<!--
+@component
+  ### Button
+  A button component, used primarily to triggers and event or action.
+
+  - @props **className** string: 'default'.
+  - @props **type** string: _'submit' | 'reset' | 'button'_.
+  - @props **onClick** Function: () => void
+  - @props **ariaLabel** string: 'Close'
+-->
+<script lang="ts">
+  // your script goes here
+  type ButtonProps = {
+    // add a optional class to the button
+    className?: string,
+    // add a DOMString behavior for type
+    type: "submit" | "reset" | "button",
+    // add a handler to be called on click
+    onClick?: () => void,
+    // add a optional string value for aria-label
+    ariaLabel?: string
+  }
+
+  export let btn: ButtonProps
+
+</script>
+
+<style lang="scss">
+  /* your styles go here */
+  button {
+		border: 1px solid var(--btn-border);
+		margin: 0;
+		padding: 0;
+		border-radius: 0;
+		background: var(--btn-bg);
+		appearance: none;
+		text-decoration: none;
+		cursor: pointer;
+    color: var(--p-text);
+    font-size: 1rem;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-right: 20px;
+    padding-left: 20px;
+
+    &:focus,
+    &:hover {
+      color: var(--btn-text-hover);
+      border-color: var(--btn-border-hover);
+    }
+	}
+</style>
+
+<!-- markup (zero or more items) goes here -->
+<button
+  class={btn.className}
+  type={btn.type}
+  on:click={btn.onClick}
+  aria-label={btn.ariaLabel}
+>
+<!-- children content -->
+  <slot></slot>
+</button>
